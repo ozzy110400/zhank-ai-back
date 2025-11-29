c35feb1ac90d2285f2283640d79ee6ae23a346d0
ozzy110400 <ozzy110400@gmail.com>
2025-11-29 17:00:05 +0100

    (feat) Pydantic models added

--- app/models.py
+++ app/models.py
@@ -1,4 +1,17 @@
 from pydantic import BaseModel
+from typing import Optional
+
+
+class Material(BaseModel):
+    name: str
+    price_per_unit: float
+    units: str
+
+
+class Product(BaseModel):
+    name: str
+    materials: list[Material]
+    total_cost: Optional[float]
 
 
 class ImageAnalysisResponse(BaseModel):
